fd43e00ff555e025ab67556f11f07fd8c8ce3a5d
Sneaky Koder <sneaky.koder@gmail.com>
2023-02-07 08:43:49 -0700

    Portrait support added

--- src/app.jsx
+++ src/app.jsx
@@ -10,6 +10,7 @@ export function App() {
   const [minute2, set_minute2] = useState("-");
   const [seconds, set_seconds] = useState(0);
   const [tick, set_tick] = useState(0);
+  const [orientation, set_orientation] = useState(null);
 
   const getHours = function () {
     return dayjs().format("hh");
@@ -43,12 +44,45 @@ export function App() {
     }
   };
 
+  const getAspectRatio = () => {
+    return window.outerWidth / window.outerHeight
+  }
+
+  const isPortrait = () => {
+    return orientation === 0 || orientation === 180;
+  };
+
   useEffect(() => {
     setInterval(() => {
       setHourDigits(getHours());
       setMinutesDigits(getMinutes());
       set_seconds(dayjs().second());
     }, 1000 * 1);
+
+    const onOrientationChange = (e) => {
+      set_orientation(e?.target?.angle || null);
+    }
+    window?.screen?.orientation?.removeEventListener('change', onOrientationChange);
+    window?.screen?.orientation?.addEventListener('change', onOrientationChange);
+
+    const onResize = (e) => {
+      if (orientation === null) {
+        setTimeout(() => {
+          set_orientation(getAspectRatio() > 1 ? 90 : 0);
+        }, 1);
+      }
+    };
+    window.removeEventListener("resize", onResize);
+    window.addEventListener("resize", onResize);
+
+    setTimeout(() => {
+      set_orientation(window?.screen?.orientation?.angle || null);
+    }, 1);
+    if (orientation === null) {
+      setTimeout(() => {
+        set_orientation(getAspectRatio() > 1 ? 90 : 0);
+      }, 1);
+    }
   }, []);
 
   useEffect(() => {
@@ -56,14 +90,24 @@ export function App() {
   }, [seconds]);
 
   return (
-    <Fragment>
-      <div class="clock" onTouchEnd={toggleFullScreen}>
-        <span class="digit hour-1">{hour1 !== "0" ? hour1 : <>&nbsp;</>}</span>
-        <span class="digit hour-2">{hour2}</span>
-        <span className={"pulse" + (tick === 1 ? "" : " black")}>:</span>
-        <span class="digit minute-1">{minute1}</span>
-        <span class="digit minute-2">{minute2}</span>
-      </div>
-    </Fragment>
+    <div className={`clock ${isPortrait() ? 'portrait' : ''}`} onTouchEnd={toggleFullScreen}>
+      <span class="digit hour-1">{hour1 !== "0" ? hour1 : <>&nbsp;</>}</span>
+      <span class="digit hour-2">{hour2}</span>
+      {
+        isPortrait() ?
+          <>
+            <span style={{ flexBasis: '100%', height: 0 }}></span>
+            <span className={`pulse ${(tick === 1 ? "" : " black")}`}>-</span>
+            <span className={`pulse ${(tick === 1 ? "" : " black")}`}>-</span>
+            <span style={{ flexBasis: '100%', height: 0 }}></span>
+          </>
+          :
+          <>
+            <span className={`pulse ${(tick === 1 ? "" : " black")}`}>:</span>
+          </>
+      }
+      <span class="digit minute-1">{minute1}</span>
+      <span class="digit minute-2">{minute2}</span>
+    </div>
   );
 }
